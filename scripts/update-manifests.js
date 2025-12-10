a38bfbf75a3e9f3b1a4e7f61e92b842c78ecde31
NIHAL  T P <nihaltpnki@gmail.com>
2025-12-10 14:33:48 +0530

    Add version update script for manifest files

---
+++ scripts/update-manifests.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+const update = (path) => {
+  const manifest = JSON.parse(fs.readFileSync(path, "utf8"));
+  manifest.version = pkg.version;
+  fs.writeFileSync(path, JSON.stringify(manifest, null, 2) + "\n");
+};
+
+update("manifests/manifest.chrome.json");
+update("manifests/manifest.firefox.json");
+
+console.log("Updated manifest versions to:", pkg.version);
